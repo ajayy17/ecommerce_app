8d68a61579b64f37c8047626d777597c45da97ef
Ajay Yadav <ajayyadav@Ajays-MacBook-Air.local>
2024-09-08 03:46:24 +0530

    category api

--- src/main/java/com/ecommerce/SBEcommApplication.java
+++ src/main/java/com/ecommerce/SBEcommApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbEcommApplication {
+public class SBEcommApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbEcommApplication.class, args);
+		SpringApplication.run(SBEcommApplication.class, args);
 	}
 
 }
